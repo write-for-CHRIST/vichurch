2bedf4b08a76f31e53b57c1eb1125b0509ca51a9
tiennt <tuantien8979@gmail.com>
2018-12-14 22:18:07 +0700

    commit code profile add

--- src/features/router/index.js
+++ src/features/router/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Home from '../home'
 import Group from '../group'
-import Profile from '../profile'
+import {ProfileAdd, ProfileList, ProfileEdit} from '../profile'
 
 const AppRouter = () => (
   <Router>
     <div>
       <Route exact path="/" exact component={Home} />
-      <Route path="/profile" exact component={Profile} />
+      <Route path="/profile/add" exact component={ProfileAdd} />
+      <Route path="/profile/edit" exact component={ProfileEdit} />
+      <Route path="/profile/list" exact component={ProfileList} />
       <Route path="/group" exact component={Group} />
     </div>
   </Router>
